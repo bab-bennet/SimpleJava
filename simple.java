0680a8111c04eece89e46e17e3a72d4d388bc4dc
bab-bennet <bennet.samuel.susaimicheal@bytesandbinaries.com>
2023-06-01 16:54:25 +0530

    added files

---
+++ simple.java
@@ -0,0 +1,9 @@
+class ben{
+    public static void main(String[] args) {
+   int  a=200;
+    int b=100;
+     int c=a-b;
+    System.out.println(c);
+     System.out.println("Hello World");
+    }
+  }
